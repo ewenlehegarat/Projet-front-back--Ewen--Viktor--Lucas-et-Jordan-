39051a3249cbb43e939d23d78eb7f610f3468ce2
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-02-17 10:47:24 +0100

    update

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 const buttonDarkMode = document.querySelectorAll('.icon_dark_mode')
+const body = document.querySelector('body')
+let darkMode = localStorage.getItem("dark_mode")
+
+const enableDarkMode = () => {
+    body.classList.remove('light_mode')
+    body.classList.add('dark_mode')
+    buttonDarkMode.forEach(btn => btn.name = 'sunny-outline')
+    localStorage.setItem("dark_mode", "enabled")
+}
+
+const disableDarkMode = () => {
+    body.classList.remove('dark_mode')
+    body.classList.add('light_mode')
+    buttonDarkMode.forEach(btn => btn.name = 'moon-outline')
+    localStorage.setItem("dark_mode", "disabled")
+}
+
+if(darkMode === "enabled"){
+    enableDarkMode()
+}
+
+buttonDarkMode.forEach(btnDarkMode => {
+    btnDarkMode.addEventListener('click', ()=>{
+        darkMode = localStorage.getItem("dark_mode")
+        if(darkMode === "disabled"){
+            enableDarkMode()
+        } else {
+            disableDarkMode()
+        }
+    })
+});
