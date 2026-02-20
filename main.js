ae3cce3991893a4eba50779c7a0adc81e3560043
Ewen Le Hegarat <ewenlehegaratpro@gmail.com>
2026-02-20 09:39:30 +0100

    //

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('nav').classList.add('view');
+});
+
+const observedElements = document.querySelectorAll('.candidatures_tables');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("view");
+      // optionnel : arrêter l’observation après l’animation
+      observer.unobserve(entry.target);
+    }
+  });
+});
+observedElements.forEach(el => observer.observe(el));
+
 const buttonDarkMode = document.querySelectorAll('.icon_dark_mode')
 const body = document.querySelector('body')
 let darkMode = localStorage.getItem("dark_mode")
